d5d80935358153b133a3047a1b3704542097d8fd
Kingseungil <flykim511@naver.com>
2023-11-15 20:25:59 +0900

    feat(auth): 로그인, 회원가입 로직 추가

---
+++ application/src/main/java/com/store/auth/controller/AuthController.java
@@ -0,0 +1,48 @@
+package com.store.auth.controller;
+
+import com.store.auth.jwt.JwtFilter;
+import com.store.auth.service.AuthService;
+import com.store.dto.AuthDto;
+import com.store.dto.AuthDto.SignUpCustomer;
+import com.store.dto.AuthDto.SignUpManager;
+import com.store.dto.TokenDto;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "AUTH")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup-customer")
+    public ResponseEntity<SignUpCustomer> signUpCustomer(
+      @Valid @RequestBody AuthDto.SignUpCustomer form) {
+        return ResponseEntity.ok(authService.signUpCustomer(form));
+    }
+
+    @PostMapping("/signup-manager")
+    public ResponseEntity<SignUpManager> signUpManager(
+      @Valid @RequestBody AuthDto.SignUpManager form) {
+        return ResponseEntity.ok(authService.signUpManager(form));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<TokenDto> signIn(@Valid @RequestBody AuthDto.SignIn form) {
+        TokenDto tokenDto = authService.signIn(form);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, JwtFilter.AUTHORIZATION_HEADER_PREFIX + tokenDto.getToken());
+
+        return ResponseEntity.ok().headers(httpHeaders).body(tokenDto);
+    }
+}
