f5f778d4a22ed201d75b32b0a56d194cab56eb38
Kingseungil <flykim511@naver.com>
2023-11-16 11:04:34 +0900

    feat(main): admin 추가

---
+++ core/src/main/java/com/store/type/UserRole.java
@@ -0,0 +1,5 @@
+package com.store.type;
+
+public enum UserRole {
+    ROLE_CUSTOMER, ROLE_MANAGER, ROLE_ADMIN
+}
--- core/src/main/java/com/store/type/UserType.java
+++
@@ -1,5 +0,0 @@
-package com.store.type;
-
-public enum UserType {
-    CUSTOMER, MANAGER
-}
