70c99591089a826715925700aa8735c8d5910bbc
Kingseungil <flykim511@naver.com>
2023-11-28 22:34:44 +0900

    단순 수정

--- application/src/main/java/com/zb/AdminInitializer.java
+++ application/src/main/java/com/zb/AdminInitializer.java
@@ -5,6 +5,7 @@ import static com.zb.type.UserRole.ROLE_ADMIN;
 import com.zb.entity.Authority;
 import com.zb.entity.Manager;
 import com.zb.repository.ManagerRepository;
+import com.zb.repository.queryDsl.ManagerQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -16,12 +17,13 @@ import org.springframework.stereotype.Component;
 public class AdminInitializer implements ApplicationRunner {
 
     private final PasswordEncoder passwordEncoder;
+    private final ManagerQueryRepository managerQueryRepository;
     private final ManagerRepository managerRepository;
 
     // Application이 실행되면서 admin 계정이 없으면 생성
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        if (managerRepository.findByUsername("admin").isEmpty()) {
+        if (managerQueryRepository.findByUsername("admin").isEmpty()) {
             managerRepository.save(Manager.builder()
                                           .username("admin")
                                           .password(passwordEncoder.encode("admin"))
--- domain/src/main/java/com/zb/repository/ManagerRepository.java
+++ domain/src/main/java/com/zb/repository/ManagerRepository.java
@@ -1,10 +1,8 @@
 package com.zb.repository;
 
 import com.zb.entity.Manager;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
 
-    Optional<Manager> findByUsername(String username);
 }
